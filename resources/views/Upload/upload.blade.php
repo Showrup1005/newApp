9c0f35ba6f2fd6db55d8306d472a615a182f8dc7
showrup1005 <showrupd@gmail.com>
2025-02-16 21:29:40 +0600

    To whom may need

--- resources/views/Upload/upload.blade.php
+++ resources/views/Upload/upload.blade.php
@@ -9,21 +9,34 @@
         <div class="col">
             <div class="card">
                 <div class="card-body">
-                    <form action="{{route('image.store')}}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <div class="mb-5">
-                            <label for="imageUpload">Name: </label>
-                            <input type="file" name="imageUpload" id="imageUpload" class="form-control">
-                            @error('imageUpload')
-                                <span class="text-danger">
-                                    {{$message}}
-                                </span>
-                            @enderror
-                        </div>
-                        <div class="mb-5">
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </div>
-                    </form>
+                    @session('success')
+                    <div class="alert alert-success" role="alert"> 
+                        {{ $value }}
+                    </div>
+                    <img src="images/{{ Session::get('image') }}" width="40%">
+                @endsession
+                
+                <form action="{{ route('image.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+            
+                    <div class="mb-3">
+                        <label class="form-label" for="inputImage">Image:</label>
+                        <input 
+                            type="file" 
+                            name="image" 
+                            id="inputImage"
+                            class="form-control @error('image') is-invalid @enderror">
+            
+                        @error('image')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+             
+                    <div class="mb-3">
+                        <button type="submit" class="btn btn-success"><i class="fa fa-save"></i> Upload</button>
+                    </div>
+                 
+                </form>
                 </div>
             </div>
         </div>
